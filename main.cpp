be5c7eebd709b1eb8f0b5bde15fe88d9e8ef48a8
Miles Smith <milesdavidsmith@gmail.com>
2022-01-20 00:17:09 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 using namespace std;
-
 #define GLM_FORCE_RADIANS
 
 #include "Scene.h"
 
-
 int main(int argc, char *argv[]) {
-
-   printf("This is the ray tracer.\n");
 
    Scene scene;
 
    scene.init();
-
    scene.draw();
-
 }
